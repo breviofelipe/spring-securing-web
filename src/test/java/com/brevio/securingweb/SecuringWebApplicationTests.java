0644e30d4f3adc1b7dc93b5ae2405b66a9274aa1
Felipe Brevio <felipebrevio05@gmail.com>
2023-07-31 22:05:30 -0300

    first commit

---
+++ src/test/java/com/brevio/securingweb/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brevio.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
