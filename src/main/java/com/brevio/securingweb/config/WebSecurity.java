5e8a6ad659acf0d844efdcec373d88a0e83f1d2f
Felipe Brevio <felipebrevio05@gmail.com>
2023-08-11 11:48:35 -0300

    Security desabilitado para metodo auth

--- src/main/java/com/brevio/securingweb/config/WebSecurity.java
+++ src/main/java/com/brevio/securingweb/config/WebSecurity.java
@@ -1,30 +1,15 @@
 package com.brevio.securingweb.config;
 
-import com.brevio.securingweb.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import static com.brevio.securingweb.config.SecurityConstants.SIGN_UP_URL;
-import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
-@EnableWebSecurity
 public class WebSecurity {
 
     private static final String ADMIN = "brevio";
@@ -33,9 +18,10 @@ public class WebSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests((authorize) -> authorize
-                        .requestMatchers("/", "/auth").permitAll()
+                        .requestMatchers("/", "/auth", "/login").permitAll()
                         .anyRequest().authenticated()
-                ).csrf(AbstractHttpConfigurer::disable);
+                )
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
